d2a567f2c7b7908f142702d1671113798e6f2bcd
saanvijarmal <itssaanvirj@gmail.com>
2026-03-23 08:38:29 +0530

    Added Harry age program

---
+++ harry_age.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int birthYear = 2000;
+    int currentYear = 2024;
+
+    int age = currentYear - birthYear;
+
+    cout << "Harry's age in 2024 is " << age;
+
+    return 0;
+}
