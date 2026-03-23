9075a70c39b51d47d8f0f461d8ae69ca9ef52032
saanvijarmal <itssaanvirj@gmail.com>
2026-03-23 08:46:15 +0530

    Added km to miles conversion program

---
+++ km_to_miles.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double km = 10.8;
+    double miles = km / 1.6;
+
+    cout << "The distance " << km << " km in miles is " << miles;
+
+    return 0;
+}
