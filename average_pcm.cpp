bf8ac8bd117da042433d5243a1a9f13dd20ef60f
saanvijarmal <itssaanvirj@gmail.com>
2026-03-23 08:43:57 +0530

    Added PCM average program

---
+++ average_pcm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+
+    double average = (maths + physics + chemistry) / 3.0;
+
+    cout << "Sam’s average mark in PCM is " << average;
+
+    return 0;
+}
